d0166314221296751fa5d9fb157cf12d6fd40711
Arthur-Van <haovvgcs200497@fpt.edu.vn>
2021-11-10 12:52:51 +0700

    Cloud Project

---
+++ apps/db.js
@@ -0,0 +1,19 @@
+var db = require("mongoose");
+
+var password="jaNJvcq5L9X6RNU4";
+var username="admin";
+
+// MongoDB link
+// mongodb+srv://admin:okMClQERuO8yRrKg@clouddb.234l0.mongodb.net/test
+// jaNJvcq5L9X6RNU4
+console.log("Connecting to database...");
+
+db.connect("mongodb+srv://admin:"+password+"@clouddata.234l0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+     useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+     console.log("Successfully connected to mongodb");
+}).catch(err=>{
+     console.log("Error found, cannot connect database" + err);
+});
+console.log("[+]: DONE");
